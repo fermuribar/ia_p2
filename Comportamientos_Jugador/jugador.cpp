c9f42bcb6d706d6b2e669231974b5fcb04f26521
fermuribar <fermuribar@correo.ugr.es>
2023-04-19 01:27:10 +0200

    Tutorial 2 forma

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -6,20 +6,240 @@
 #include <set>
 #include <stack>
 
+//CONSTRUCTOR-----------------------------------------------------------------------------------------------------------
+
+ComportamientoJugador::ComportamientoJugador(unsigned int size) : Comportamiento(size) {
+    // Inicializar Variables de Estado
+	hayPlan = false;
+}
+
+
+//THINK------------------------------------------------------------------------------------------------------------------
+
+//movimiento caballo
+list<Action> caballito(){
+	list<Action> secuencia;
+	secuencia.push_back(actFORWARD);
+	secuencia.push_back(actFORWARD);
+	secuencia.push_back(actTURN_R);
+	secuencia.push_back(actFORWARD);
+	return secuencia;
+}
+
+//primera aproximacion a la implementacion de la busqueda en anchura;
+
+
+
 // Este es el método principal que se piden en la practica.
 // Tiene como entrada la información de los sensores y devuelve la acción a realizar.
 // Para ver los distintos sensores mirar fichero "comportamiento.hpp"
-Action ComportamientoJugador::think(Sensores sensores)
-{
-	Action accion = actIDLE;
+Action ComportamientoJugador::think(Sensores sensores){
+	Action accion= actIDLE;
+
 
 	// Incluir aquí el comportamiento del agente jugador
+
+	if(!hayPlan){
+		//invocamos  el método de búsqueda
+		cout << "Calculando un nuevo plan" << endl;
+
+		//estado actual
+		c_state.jugador.f = sensores.posF;
+		c_state.jugador.c = sensores.posC;
+		c_state.jugador.brujula = sensores.sentido;
+		c_state.sonambulo.f = sensores.SONposF;
+		c_state.sonambulo.c = sensores.SONposC;
+		c_state.sonambulo.brujula = sensores.SONsentido;
+		
+		//objetivo
+		goal.f = sensores.destinoF;
+		goal.c = sensores.destinoC;
+
+		plan = AnchuraSoloJugador();
+		VisualizaPlan(c_state,plan);
+		hayPlan = true;
+	}
+
+	if(hayPlan and plan.size() > 0){
+		cout << "Ejecutando la siguiente acción del plan" << endl;
+		accion = plan.front();
+		plan.pop_front();
+	}
+
+	if(plan.size() == 0){
+		cout << "Se completó el plan" << endl;
+		hayPlan = false;
+	}
+	
 
 	return accion;
 }
 
-
 int ComportamientoJugador::interact(Action accion, int valor)
 {
 	return false;
+}
+
+//_____________________________________Representacion del plan en simulador_______________________
+//borra matriz
+void ComportamientoJugador::AnulaMatriz(){
+	for(int i = 0; i < mapaConPlan.size(); i++){
+		for(int j = 0; j < mapaConPlan.size(); j++){
+			mapaConPlan[i][j] = 0;
+		}
+	}
+}
+//visualiza el plan en el simulador
+void ComportamientoJugador::VisualizaPlan(const stateN0& st, const list<Action>& plan){
+	AnulaMatriz();
+	stateN0 cst = st;
+	auto it = plan.begin();
+	while(it != plan.end()){
+		switch (*it){
+			case actFORWARD:
+				cst.jugador = NextCasilla(cst.jugador);
+				mapaConPlan[cst.jugador.f][cst.jugador.c] = 1;
+			break;
+			case actTURN_R:
+				cst.jugador.brujula = static_cast<Orientacion>((cst.jugador.brujula + 2) % 8);
+			break;
+			case actTURN_L:
+				cst.jugador.brujula = static_cast<Orientacion>((cst.jugador.brujula + 6) % 8);
+			break;
+			case actSON_FORWARD:
+				cst.sonambulo = NextCasilla(cst.sonambulo);
+				mapaConPlan[cst.sonambulo.f][cst.sonambulo.c] = 1;
+			break;
+			case actSON_TURN_SR:
+				cst.sonambulo.brujula = static_cast<Orientacion>((cst.sonambulo.brujula + 1) % 8);
+			break;
+			case actSON_TURN_SL:
+				cst.sonambulo.brujula = static_cast<Orientacion>((cst.sonambulo.brujula + 7) % 8);
+			break;
+		}
+		it++;
+	}
+}
+
+
+//___________________________________________Buesquedas___________________________________________
+
+//-----------------------------------------------N0-----------------------------------------------
+
+//busca un estado en una lista de estados
+bool ComportamientoJugador::Find(const stateN0& item, const list<nodoN0>& lista){
+	auto it = lista.begin();
+	while(it != lista.end() and !(it->st == item)){
+		it++;
+	}
+	return (it != lista.end());
+}
+
+//obtenfo cual es la casilla si abanzo segun su ubicacion
+ubicacion ComportamientoJugador::NextCasilla(const ubicacion& pos){
+	ubicacion next = pos;
+	switch (pos.brujula)	{
+		case norte:
+			next.f--;
+		break;
+		case noreste:
+			next.f--;
+			next.c++;
+		break;
+		case este:
+			next.c++;
+		break;
+		case sureste:
+			next.f++;
+			next.c++;
+		break;
+		case sur:
+			next.f++;
+		break;
+		case suroeste:
+			next.f++;
+			next.c--;
+		break;
+		case oeste:
+			next.c--;
+		break;
+		case noroeste:
+			next.f--;
+			next.c--;
+		break;
+	}
+
+	return next;
+}
+
+//aplica una accion a un estado y obtiene otro
+stateN0 ComportamientoJugador::apply(const Action& a, const stateN0& st){
+	stateN0 st_result = st;
+	ubicacion sig_ubicacion;
+	switch (a){
+		case actFORWARD:
+			sig_ubicacion = NextCasilla(st.jugador);
+			if(CasillaTransitable(sig_ubicacion) and !(sig_ubicacion == st.sonambulo)) st_result.jugador = sig_ubicacion;
+		break;
+		case actTURN_L:
+			st_result.jugador.brujula = static_cast<Orientacion>((st.jugador.brujula + 6) % 8);
+		break;
+		case actTURN_R:
+			st_result.jugador.brujula = static_cast<Orientacion>((st.jugador.brujula + 2) % 8);
+		break;
+	}
+	return st_result;
+}
+
+//busqueda en anchura obteniendo lista de acciones
+list<Action> ComportamientoJugador::AnchuraSoloJugador(){
+	nodoN0 current_node;
+	list<nodoN0> frontier;
+	list<nodoN0> explored;
+	list<Action> plan;
+	current_node.st = c_state;
+	bool SolutionFound = (current_node.st.jugador.f == goal.f and current_node.st.jugador.c == goal.c);
+
+	frontier.push_back(current_node);
+
+	while(!frontier.empty() and !SolutionFound){
+		frontier.pop_front();
+		explored.push_back(current_node);
+		//Generar hijo actForward
+		nodoN0 child_forward = current_node;
+		child_forward.st = apply(actFORWARD, current_node.st);
+		if(child_forward.st.jugador.f == goal.f and child_forward.st.jugador.c == goal.c){
+			child_forward.secuencia.push_back(actFORWARD);
+			current_node = child_forward;
+			SolutionFound = true;
+		}else if(!Find(child_forward.st,frontier) and !Find(child_forward.st,explored)){
+			child_forward.secuencia.push_back(actFORWARD);
+			frontier.push_back(child_forward);
+		}
+
+		if(!SolutionFound){
+			//Generar hijo actTurn_L
+			nodoN0 child_turnl = current_node;
+			child_turnl.st = apply(actTURN_L,current_node.st);
+			if(!Find(child_turnl.st,frontier) and !Find(child_turnl.st,explored)){
+				child_turnl.secuencia.push_back(actTURN_L);
+				frontier.push_back(child_turnl);
+			}
+			//Generar hijo actTurn_R
+			nodoN0 child_turnr = current_node;
+			child_turnr.st = apply(actTURN_R,current_node.st);
+			if(!Find(child_turnr.st,frontier) and !Find(child_turnr.st,explored)){
+				child_turnr.secuencia.push_back(actTURN_R);
+				frontier.push_back(child_turnr);
+			}
+		}
+
+		if(!SolutionFound and !frontier.empty()) current_node = frontier.front();
+
+		
+	}
+
+	if(SolutionFound) plan = current_node.secuencia;
+
+	return plan;
 }
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -5,12 +5,46 @@
 
 #include <list>
 
+//Estado
+struct stateN0{
+  ubicacion jugador;
+  ubicacion sonambulo;
+
+  bool operator== (const stateN0& x) const{
+    if(jugador == x.jugador and sonambulo.f ==  x.sonambulo.f and sonambulo.c == x.sonambulo.c){
+      return true;
+    }else{
+      return false;
+    }
+  }
+};
+
+//Nodo
+
+struct nodoN0{
+  stateN0 st;
+  list<Action> secuencia;
+
+  bool operator== (const nodoN0& n) const{
+    return (st == n.st);
+  }
+
+  bool operator< (const nodoN0& n) const{
+    if(st.jugador.f < n.st.jugador.f) return true;
+    else if(st.jugador.f == n.st.jugador.f and st.jugador.c < n.st.jugador.c) return true;
+    else if(st.jugador.f == n.st.jugador.f and st.jugador.c == n.st.jugador.c and st.jugador.brujula < n.st.jugador.brujula) return true;
+    else return false;
+  }
+
+};
+
+
+
 
 class ComportamientoJugador : public Comportamiento {
   public:
-    ComportamientoJugador(unsigned int size) : Comportamiento(size) {
-      // Inicializar Variables de Estado
-    }
+    ComportamientoJugador(unsigned int size);
+
     ComportamientoJugador(std::vector< std::vector< unsigned char> > mapaR) : Comportamiento(mapaR) {
       // Inicializar Variables de Estado
     }
@@ -22,12 +56,33 @@ class ComportamientoJugador : public Comportamiento {
 
 
   private:
-    // Declarar Variables de Estado
+    // Declaracion de metodos privados________________________________________
 
+    //--------------Representacion del plan en simulador----------------------
+      //borra matriz
+    void AnulaMatriz();
+      //visualiza el plan en el simulador
+    void VisualizaPlan(const stateN0& st, const list<Action>& plan);
 
+    //--------------Busqueda y subfunciones-----------------------------------
+      //busca un estado en una lista de estados
+    bool Find(const stateN0& item, const list<nodoN0>& lista);
+      //obtenfo cual es la casilla si abanzo segun su ubicacion
+    ubicacion NextCasilla(const ubicacion& pos);
+      //aplica una accion a un estado y obtiene otro
+    stateN0 apply(const Action& a, const stateN0& st);
+      //comprueba si la casilla es transitable o no
+    bool inline CasillaTransitable(const ubicacion& x){ return (mapaResultado[x.f][x.c] != 'M' and mapaResultado[x.f][x.c] != 'P');}
+      //busqueda en anchura obteniendo lista de acciones
+    list<Action> AnchuraSoloJugador();
 
+    // Declarar Variables de Estado___________________________________________
 
+    list<Action> plan;  //Almacena el plan en ejecución
+    bool hayPlan;       //Si verdad indica que se está siguiendo un plan.
 
+    stateN0 c_state;    //estado actual
+    ubicacion goal;     //ubicacion del objetivo
 };
 
 #endif
